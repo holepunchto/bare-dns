852ccaff8bb81609fd73a618f1034b040785a646
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-01-23 09:49:00 +0100

    Improve types

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,29 @@
+type IPFamily = 4 | 6
+
+interface LookupOptions {
+  family?: `IPv${IPFamily}` | IPFamily | 0
+}
+
 declare namespace dns {
-  interface LookupOptions {
-    family?: string | number
-  }
+  export { IPFamily, LookupOptions }
 
   export function lookup(
     hostname: string,
-    cb: (err: Error | null, address: string, family: number) => void
+    cb: (
+      err: Error | null,
+      address: string | null,
+      family: IPFamily | 0
+    ) => void
   ): void
 
   export function lookup(
     hostname: string,
     opts: LookupOptions & { all?: false },
-    cb: (err: Error | null, address: string, family: number) => void
+    cb: (
+      err: Error | null,
+      address: string | null,
+      family: IPFamily | 0
+    ) => void
   ): void
 
   export function lookup(
@@ -19,7 +31,7 @@ declare namespace dns {
     opts: LookupOptions & { all: true },
     cb: (
       err: Error | null,
-      addresses: { address: string; family: number }[]
+      addresses: { address: string; family: IPFamily }[] | null
     ) => void
   ): void
 }
